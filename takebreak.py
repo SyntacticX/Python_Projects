fd68329746b36b14e2f48e634b81ffe7f4a759db
SyntacticX <rmm54@njit.edu>
2015-05-25 11:40:37 -0400

    initial commit of takebreak.py

---
+++ takebreak.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+
+print("This program encourages you to take frequent breaks throughout the work day.")
+hours = input("Please input the number of hours you'll be working today: ")
+print("See you in two hours.")
+
+numberOfBreaks = hours/2
+
+for x in xrange(0,numberOfBreaks):
+    time.sleep(2*60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=zq7Eki5EZ8o")
